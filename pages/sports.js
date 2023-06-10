dc4f95b191eed817000f5d3a0ff3655295bd3d3b
tim345tim <tim704e@outlook.com>
2023-06-10 17:23:54 -0500

    fixed undefined map error streamlined

--- pages/sports.js
+++ pages/sports.js
@@ -1,35 +1,62 @@
+// import Head from 'next/head';
+// import ArticleCard from '@/components/ArticleCard';
+
+// export async function getStaticProps() {
+//   const articles = await getArticles();
+//   return { 
+//     props: { articles },
+//     revalidate: 86400,
+//   };
+// }
+
+// function stripArticle(article) {
+//   return {
+//       title: article.title, 
+//       description: article.description, 
+//       url: article.url,
+//   };
+// }
+
+// export async function getArticles() {
+//   const api_key = process.env.API_KEY;
+//   const response = await fetch(`http://api.mediastack.com/v1/news?access_key=${api_key}&limit=20&languages=en&categories=sports`);
+//   const responseJSON = await response.json();
+//   const articles = await responseJSON["data"];
+//   // return articles.map(stripArticle);
+//   return articles;
+// }
+
+// function SportsPage({ articles }) {
+//   return (
+//     <>
+//       <Head>
+//         <title>News!</title>
+//       </Head>
+//       <main className="px-6 py-4 bg-slate-100">
+//         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+//           {articles && articles.map((article, index) => (
+//               <ArticleCard article={article} key={index} />
+//           ))}
+//         </ul>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default SportsPage;
+
 import Head from 'next/head';
 import ArticleCard from '@/components/ArticleCard';
 
 export async function getStaticProps() {
-  const articles = await getArticles();
-  // return { 
-  //   props: { articles },
-  //   revalidate: 86400,
-  // };
-  return { 
-    props: { 
-      articles: JSON.parse(JSON.stringify(articles))
-     },
-    revalidate: 86400,
-  };
-}
-
-function stripArticle(article) {
-  return {
-      title: article.title, 
-      description: article.description, 
-      url: article.url,
-  };
-}
-
-export async function getArticles() {
   const api_key = process.env.API_KEY;
   const response = await fetch(`http://api.mediastack.com/v1/news?access_key=${api_key}&limit=20&languages=en&categories=sports`);
   const responseJSON = await response.json();
   const articles = await responseJSON["data"];
-  // return articles.map(stripArticle);
-  return articles;
+  return { 
+    props: { articles },
+    revalidate: 86400,
+  };
 }
 
 function SportsPage({ articles }) {
